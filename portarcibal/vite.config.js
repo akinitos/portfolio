7f82152f86e118f797aa040e86a60be641ce94ac
akinitos <vonarcibal@gmail.com>
2025-07-09 19:57:55 +0800

    sana?

--- portarcibal/vite.config.js
+++ portarcibal/vite.config.js
@@ -1,11 +1,11 @@
+// portfolio/vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()], 
-  base: mode === 'production' ? '/portfolio/portarcibal/' : '/',
-  build: {
-    outDir: 'docs',
-  },
-}));
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Add this line:
+  base: '/portfolio/',
+})
