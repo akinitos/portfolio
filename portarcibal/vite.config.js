36f24c8c821d3c7828cd74931b3d58d8d4f481d3
akinitos <vonarcibal@gmail.com>
2025-07-10 00:40:14 +0800

    v

--- portarcibal/vite.config.js
+++ portarcibal/vite.config.js
@@ -1,13 +1,13 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/portfolio/' : '/',
-  build: {
-    outDir: 'dist',
+  resolve: {
+    alias: { '@': path.resolve(__dirname, 'src') }
   },
-  server: {
-    historyApiFallback: true,
-  }
+  base: mode === 'production' ? '/portfolio/' : '/',
+  build: { outDir: 'docs' },
+  server: { historyApiFallback: true },
 }))
