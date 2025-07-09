b426e065b264b8b1413f2be34316c039b1e6cc3e
akinitos <vonarcibal@gmail.com>
2025-07-09 20:19:49 +0800

    f

--- portarcibal/vite.config.js
+++ portarcibal/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()], 
-  base: mode === 'production' ? '/portfolio/portarcibal/' : '/',
+  base: mode === 'production' ? '/portfolio/' : '/',
   build: {
     outDir: 'docs',
   },
